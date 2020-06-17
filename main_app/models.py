61816e717f9dd8c327d220e8b9c33e7f9bbad3f7
Kimberly Lord <kalord93@gmail.com>
2020-06-17 12:18:21 -0600

    Add Chinchilla model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Chinchilla(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'chinchilla_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Chinchilla
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def chinchillas_index(request):
+    chinchillas = Chinchilla.objects.order_by('-id')
+    return render(request, 'chinchillas/index.html', {'chinchillas': chinchillas})
+
